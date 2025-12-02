a87eeb4d14d0bdf351985fa532d0b1493aeb332d
SeamDux <seba.merino@duocuc.cl>
2025-12-02 16:25:21 -0300

    Ajustes de texto mes de maria

--- app/(app)/devociones-maria/mes-maria.tsx
+++ app/(app)/devociones-maria/mes-maria.tsx
@@ -23,17 +23,44 @@ export default function MesMariaScreen() {
           <Text style={styles.subtitulo}>Oración Inicial</Text>
 
           <Text style={styles.oracion}>
-            Oh María, durante el bello mes que te está consagrado, todo resuena con tu nombre y alabanza! Tu santuario resplandece con nuevo brillo y nuestras manos te han elevado a un trono de gracia y amor, desde donde presides nuestras fiestas y escuchas nuestras oraciones y votos. Para honrarte hemos esparcido frescas flores a tus pies y tu vuestro frente con guirnaldas y coronas. Más ¡Oh María! no te das por satisfecha con estos homenajes; hay flores cuya frescura y lozanía jamás pasan y coronas que no se marchitan. Estas son las que tu esperas de tus hijos; porque el más hermoso adorno de una madre es la piedad de sus hijos; y la más bella corona que pueden deponer a sus pies es la de sus virtudes. Sí, los hijos que tu nos pedís son la inocencia de nuestros corazones, nos esforzaremos, pues, durante el curso de este mes consagrado a tu gloria, oh Virgen Santa, en conservar nuestras almas puras y sin manchas y en separar de nuestros pensamientos, deseos y miradas aun la sombra misma del mal; la rosa cuyo brillo agrada a tus ojos es la caridad, el amor a Dios y a nuestros hermanos nos amaremos, pues, los unos a los otros, como hijos de una misma familia cuya madre eres, viviendo todos en la dulzura de una concordia fraternal. En este mes bendito procuraremos cultivar en nuestros corazones la humildad, modesta flor que te es tan querida, y con tu auxilio llegaremos a ser puros, humildes, caritativos, pacientes y resignados. ¡Oh María! Haz producir en el fondo de nuestros corazones todas estas amables virtudes.
+            Oh María, durante el bello mes que te está consagrado, 
+            todo resuena con tu nombre y alabanza! 
+            Tu santuario resplandece con nuevo brillo y nuestras manos te han elevado a un trono de gracia y amor, 
+            desde donde presides nuestras fiestas y escuchas nuestras oraciones y votos. 
+            Para honrarte hemos esparcido frescas flores a tus pies y tu vuestro frente con guirnaldas y coronas. 
+            Más ¡Oh María! no te das por satisfecha con estos homenajes; 
+            hay flores cuya frescura y lozanía jamás pasan y coronas que no se marchitan. 
+            Estas son las que tu esperas de tus hijos; porque el más hermoso adorno de una madre es la piedad de sus hijos; 
+            y la más bella corona que pueden deponer a sus pies es la de sus virtudes. 
+            Sí, los lirios que tu nos pides son la inocencia de nuestros corazones, 
+            nos esforzaremos, pues, durante el curso de este mes consagrado a tu gloria, oh Virgen Santa, 
+            en conservar nuestras almas puras y sin manchas y en separar de nuestros pensamientos, 
+            deseos y miradas aun la sombra misma del mal; la rosa cuyo brillo agrada a tus ojos es la caridad, 
+            el amor a Dios y a nuestros hermanos nos amaremos, pues, los unos a los otros, 
+            como hijos de una misma familia cuya madre eres, viviendo todos en la dulzura de una concordia fraternal. 
+            En este mes bendito procuraremos cultivar en nuestros corazones la humildad, 
+            modesta flor que te es tan querida, y con tu auxilio llegaremos a ser puros, humildes, caritativos, pacientes y resignados. 
+            ¡Oh María! Haz producir en el fondo de nuestros corazones todas estas amables virtudes.
           </Text>
 
           <Text style={styles.oracion}>
-            Que ellas broten, florezcan y den al fin frutos de gracia, para poder ser algún día dignos hijos de la más santa y la mejor de las madres. Amén.
+            Que ellas broten, florezcan y den al fin frutos de gracia, 
+            para poder ser algún día dignos hijos de la más santa y la mejor de las madres. Amén.
           </Text>
 
           <Text style={styles.subtitulo}>Oración final</Text>
 
           <Text style={styles.oracion}>
-            ¡Oh María!, Madre de Jesús, nuestro Salvador y nuestra buena Madre! Nosotros venimos a ofrecerte, con estos obsequios que colocamos a tus pies, nuestros corazones, deseosos de serte agradables, y a solicitar de tu bondad un nuevo ardor en tu santo servicio. Dígnate presentarnos a tu Divino Hijo, que en vista de sus méritos y a nombre de su Santa Madre, dirija nuestros pasos por el sendero de la virtud, que haga lucir con nuevo esplendor la luz de la fe sobre los infortunados pueblos que gimen por tanto tiempo en las tinieblas del error, que vuelvan hacia Él, y cambie tantos corazones rebeldes, cuya penitencia regocijará su corazón y el tuyo. Que convierta a los enemigos de su Iglesia y que en fin, encienda por todas partes el fuego de su ardiente caridad, que nos colme de alegría en medio de las tribulaciones de esta vida, y de esperanza para el porvenir. Amén.
+            ¡Oh María!, Madre de Jesús, nuestro Salvador y nuestra buena Madre! 
+            Nosotros venimos a ofrecerte, con estos obsequios que colocamos a tus pies, 
+            nuestros corazones, deseosos de serte agradables, 
+            y a solicitar de tu bondad un nuevo ardor en tu santo servicio. 
+            Dígnate presentarnos a tu Divino Hijo, que en vista de sus méritos y a nombre de su Santa Madre, 
+            dirija nuestros pasos por el sendero de la virtud, 
+            que haga lucir con nuevo esplendor la luz de la fe sobre los infortunados pueblos que gimen por tanto tiempo en las tinieblas del error, 
+            que vuelvan hacia Él, y cambie tantos corazones rebeldes, cuya penitencia regocijará su corazón y el tuyo. 
+            Que convierta a los enemigos de su Iglesia y que en fin, encienda por todas partes el fuego de su ardiente caridad, 
+            que nos colme de alegría en medio de las tribulaciones de esta vida, y de esperanza para el porvenir. Amén.
           </Text>
 
           <Text style={styles.cita}>
